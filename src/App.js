72d716474c74a21883fde3ee7c5be987823acf24
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2021-01-14 21:24:39 +0530

    wip: add database of books

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import React from "react";
 import "./styles.css";
 
-const bookshrlf = {};
+const bookshelf = {
+  Fantasy: [
+    {
+      name: "A song of Ice and Fire",
+      rating: "4.5/5",
+      author: "George R.R Martin"
+    },
+    {
+      name: "Harry Potter and the Philosophers Stone",
+      rating: "5/5",
+      author: "J.K. Rowling"
+    }
+  ],
+  Biographies: [
+    {
+      name: "Wings of fire",
+      rating: "4.6/5",
+      author: "Dr. A.P.J. Abdul Kalam and Arun Tiwari"
+    },
+    {
+      name: "A promised land",
+      rating: "4.4/5",
+      author: "Barack Obama"
+    }
+  ],
+  Productivity: [
+    {
+      name: "Atomic Habits",
+      rating: "4.6/5",
+      author: "James Clear"
+    },
+    {
+      name: "Deep Work",
+      rating: "4.5/5",
+      author: "Cal Newport"
+    }
+  ]
+};
 
 export default function App() {
   return (
